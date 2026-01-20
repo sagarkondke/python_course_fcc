91d9666b14c9c3f4ff215bacf2b0da6f866b4e56
sagar kondke <kondkesagar@gmail.com>
2026-01-20 18:43:59 +0530

    DAY 1

---
+++ BasicD1.py
@@ -0,0 +1,14 @@
+name = 'John Doe'
+age = 26
+name_and_age = f'My name is {name} and I am {age} years old'
+print(name_and_age) # My name is John Doe and I am 26 years old
+
+num1 = 5
+num2 = 10
+print(f'The sum of {num1} and {num2} is {num1 + num2}')
+
+
+name ='sagar k'
+age= 25
+name_ang_age= f'my name is {name} and i am {age} yers old'
+print(name_ang_age)
