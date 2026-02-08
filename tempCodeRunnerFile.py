cb9e5ee82be3acbe07d33d67e03e8cdb0d2f7610
sagar kondke <kondkesagar@gmail.com>
2026-02-08 12:27:17 +0530

    Day complate

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,7 @@
-a=(0,0,0,0)
-print(a.count(0))
+post=input('talking about to sagar')
+p ='Sagar is not doing hard work i ' \
+'am become making lazy'
+if (post in p ):
+    print('post is talking about sagar ')
+else:
+    print('its in not list')
