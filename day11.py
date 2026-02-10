39ed672c0049d0f9ba621241f083cfca50aceb77
sagar kondke <kondkesagar@gmail.com>
2026-02-10 15:16:02 +0530

    Day complate

--- day11.py
+++ day11.py
@@ -1,1 +1,56 @@
-#new day add 
+# loops 
+for i in range(1,6):
+    print(i)
+
+
+# while loops 
+i =1
+while(i<6):
+    print(i)
+    i+=1
+
+
+# write a program to print 1 to 50 using while loops.
+i =1
+while (i<50):
+    print(i)
+    i+=1
+
+
+# Write a program to print the content of a list using 
+# while loop 
+
+l=[1,'sagar','vinod',4,5,6 ]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i+=1
+
+# FOR Loops 
+
+for i in range(4):
+    print(i)
+
+
+# how to list use in python 
+l=[1,3,5,3,5,6]
+for i in l:
+    print(i)
+
+
+# how to string in loops in python 
+s ='sagar'
+for i in s:
+    print(i)
+
+
+# ## VVIP 
+# For loops in else
+l=['sagar','harry']
+for item in l:
+    print(item)
+else:
+    print('done')
+
+
+
