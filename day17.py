08b39ced58070305dea798a9984f6eed58a374dd
sagar kondke <kondkesagar@gmail.com>
2026-02-18 09:10:35 +0530

    day 17 complate

---
+++ day17.py
@@ -0,0 +1,16 @@
+class employee:
+    language='py'
+    salary=1200000
+
+harry=employee() # harry is object here
+harry.name='sagar'
+print(harry.name,harry.language,harry.salary)
+
+rohan = employee()
+rohan.name='rohan'
+print(rohan.name,rohan.salary,rohan.language)
+
+# here name is object attribute and salary and language are
+# class attributes as they directly belong to the class.
+
+
